c5753fb06004930fb834da16de0b05c1ebb2c5d4
Thisas <thisas@protonmail.com>
2025-04-25 23:15:26 +0400

    make fixes for c++14

--- build.sh
+++ build.sh
@@ -3,22 +3,23 @@ echo "Configuring and building Thirdparty/DBoW2 ..."
 cd Thirdparty/DBoW2
 mkdir build
 cd build
+apt install libboost-dev 
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j
-
-cd ../../g2o
+cd ../../
 
 echo "Configuring and building Thirdparty/g2o ..."
 
+cd g2o
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j
-
-cd ../../Sophus
+cd ../../
 
 echo "Configuring and building Thirdparty/Sophus ..."
 
+cd Sophus
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
@@ -32,9 +33,17 @@ cd Vocabulary
 tar -xf ORBvoc.txt.tar.gz
 cd ..
 
+# echo "Installing Pangolin ..."
+# cd Pangolin
+# cmake -B build -GNinja
+# cmake --build build
+# cd ..
+
 echo "Configuring and building ORB_SLAM3 ..."
 
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+apt update
+apt install -y libopencv-dev
+cmake .. -DCMAKE_BUILD_TYPE=Release  -DPangolin_DIR=/ORB_SLAM3/Pangolin/build
 make -j4
