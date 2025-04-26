2149b52eab34fe44fb85325c6c7aa27f514c74c4
Thisas <thisas@protonmail.com>
2025-04-26 13:19:45 +0400

    fixed build script

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
+echo "Installing Pangolin ..."
+cd Pangolin/
+./scripts/install_prerequisites.sh recommended -u -y
+apt install python3-pip -y
+cmake -B build -GNinja
+cmake --build build
+cd ..
+
 echo "Configuring and building Thirdparty/DBoW2 ..."
 
 cd Thirdparty/DBoW2
@@ -5,7 +13,7 @@ mkdir build
 cd build
 apt update
 apt install -y libboost-dev libopencv-dev
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-w"
 make -j1
 cd ../../
 
@@ -14,7 +22,7 @@ echo "Configuring and building Thirdparty/g2o ..."
 cd g2o
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-w"
 make -j1
 cd ../../
 
@@ -23,7 +31,7 @@ echo "Configuring and building Thirdparty/Sophus ..."
 cd Sophus
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-w"
 make -j1
 
 cd ../../../
@@ -34,19 +42,11 @@ cd Vocabulary
 tar -xf ORBvoc.txt.tar.gz
 cd ..
 
-# echo "Installing Pangolin ..."
-# cd Pangolin
-# cmake -B build -GNinja
-# cmake --build build
-# cd ..
-
 echo "Configuring and building ORB_SLAM3 ..."
 
 mkdir build
 cd build
 apt update
-cmake .. -DCMAKE_BUILD_TYPE=Release  -DPangolin_DIR=/ORB_SLAM3/Pangolin/build
+apt install libboost-serialization-dev -y
+cmake .. -DCMAKE_BUILD_TYPE=Release  -DPangolin_DIR=/ORB_SLAM3/Pangolin/build -DCMAKE_CXX_FLAGS="-w"
 make -j1
-
-# for no warnings 
-# cmake .. -DCMAKE_BUILD_TYPE=Release -DPangolin_DIR=/ORB_SLAM3/Pangolin/build -DCMAKE_CXX_FLAGS="-w"
